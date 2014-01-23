a3af8d6a109ade1289109b287418db27497514bc
Angel Ochoa <angelv8a8@hotmail.com>
2014-01-23 17:59:06 -0600

    Demo 4
    Subir demo 4

---
+++ Demo4/src/com/example/demo4/ShowSearchQueryActivity.java
@@ -0,0 +1,45 @@
+package com.example.demo4;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class ShowSearchQueryActivity extends Activity {
+
+	public static final String QUERY = "query";
+	
+	
+	private static final String TAG = ShowSearchQueryActivity.class.toString();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_show_search_query);
+		
+		Intent intent = getIntent();
+		
+		String query = intent.getStringExtra(QUERY);
+		
+		if(query != null)
+		{
+			Log.i(TAG, query);
+			TextView textView = (TextView)findViewById(R.id.textView1);
+			textView.setText(query);
+			
+		}else
+		{
+			Log.i(TAG, "No query");
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.show_search_query, menu);
+		return true;
+	}
+
+}
